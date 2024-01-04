52954154a07464af4fab3a53058125b0644c092c
QiaoKe201512 <2627296432@qq.com>
2024-01-04 17:24:41 +0800

    测试

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import paramiko
 
 from backend.common.host import Host
 from backend.common.script import Script
+from backend.script.execute.execute import connect_and_execute_script
 
 if __name__ == "__main__":
     # 远程服务器的 hostname、port、username 和 password
@@ -11,4 +12,9 @@ if __name__ == "__main__":
     password = "vd0N4ERzV51qwFB8uZXo"
 
     host = Host(hostname, port, username, password)
-    script = Script()
+    script = Script(script_type="shell", script_url=r"F:\code\python\infra-auto\backend\script\test.sh", script_host_path="/tmp/test.sh", script_id=1, name="test", description="test")
+
+    res, err = connect_and_execute_script(host, script)
+
+    print(res)
+    print(err)
